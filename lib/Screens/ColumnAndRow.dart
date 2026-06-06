72f041fa372b2f5d0ed3ef9604f7706f1117d598
FabricioSG1 <fs22436@gmail.com>
2026-06-06 14:01:50 -0400

    Column & Row Fase 3

--- lib/Screens/ColumnAndRow.dart
+++ lib/Screens/ColumnAndRow.dart
@@ -1,115 +1,256 @@
 import 'package:flutter/material.dart';
 
 class ColumnAndRow extends StatefulWidget {
+  
   const ColumnAndRow({super.key});
 
   @override
   State<ColumnAndRow> createState() => _ColumnAndRowState();
 }
-
 class _ColumnAndRowState extends State<ColumnAndRow> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Pantalla Column y Row"),
         backgroundColor: const Color.fromARGB(255, 0, 150, 110),
-        foregroundColor: const Color.fromARGB(255, 231, 231, 231),
+        foregroundColor: Color.fromARGB(255, 231, 231, 231),
+
       ),
+
       body: Padding(
-        padding: const EdgeInsets.all(1),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: Center(
-                child: Column(
-                  children: [
-                    Container(
-                      width: 60,
-                      height: 60,
-                    color: Colors.amber,
-                      child: const Text("A"),
-                    ),
-                    Container(
-                      width: 90,
-                      height: 90,
-                    color: Colors.green,
-                      child: const Text("B"),
-                    ),
-                    Container(
-                      width: 60,
-                      height: 60,
-                      color: Colors.blue,
-                      child: const Text("C"),
-                    ),
-                  ],
+  padding: const EdgeInsets.all(1),
+  child: Column(
+    children: [
+
+      Expanded(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+                child: const Text("A",
+                    style: TextStyle(
+                    fontSize: 18,
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      const Text(
-                        "Modo:",
-                        style: TextStyle(),
-                      ),
-                      const Text("Column"),
-                      Switch(
-                        value: false,
-                        onChanged: (value) {},
-                      ),
-                      const Text("Row"),
-                    ],
+
+              Container(
+                width: 90,
+                height: 90,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                    ),
                   ),
-                  const Text("mainAxisAlignment"),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("start"),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("center"),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("end"),
-                      ),
-                    ],
+                child: const Text("B",
+                style: TextStyle(
+                    fontSize: 18,
                   ),
-                  const SizedBox(height: 25),
-                  const Text(
-                    "crossAxisAlignment:",
-                    style: TextStyle(),
-                  ),
-                  const SizedBox(height: 15),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("start"),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("center"),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("end"),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
+
+              Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ), 
+                ),
+                child: const Text("C",
+                    style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
             ),
           ],
         ),
       ),
+    ),
+
+      Expanded(child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Text(
+                  "Modo:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                const Spacer(),
+                const Text("Column"),
+                Switch(
+                  value: false,
+                  onChanged: (value) {},
+                ),
+                const Text("Row"),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text("mainAxisAlignment",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      side: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: const Text("start"),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      side: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: const Text("center"),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      side: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: const Text("end"),
+                ),
+              ],
+            ),
+            
+            const SizedBox(height: 25),
+            const Text("crossAxisAlignment:",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      side: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: const Text("start"),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      side: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: const Text("center"),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      side: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: const Text("end"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      ),
+    ],
+  ),
+),
     );
   }
 }
