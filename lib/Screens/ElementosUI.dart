593c31798b28b8e71e7369c1306533a469d6b4fd
FabricioSG1 <fs22436@gmail.com>
2026-06-06 21:01:17 -0400

    Elementos UI Fase 4

--- lib/Screens/ElementosUI.dart
+++ lib/Screens/ElementosUI.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 class ElementosUI extends StatefulWidget {
   const ElementosUI({super.key});
+  
 
   @override
   State<ElementosUI> createState() => _ElementosUI();
+  
 }
 
 class _ElementosUI extends State<ElementosUI> {
+  double fontSize = 38;
+  bool isBold = false;
+  bool isItalic = false;
+  Color color = Colors.blue;
+  int alignmentIndex = 1; 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,42 +29,56 @@ class _ElementosUI extends State<ElementosUI> {
   padding: const EdgeInsets.all(20),
   child: Column(
     children: [
-      const Expanded(
-        child: Center(
-          child: Text(
-            "Hola Flutter",
-            style: TextStyle(
-              fontSize: 38,
-              color: Colors.blue,
-            ),
-          ),
+       Expanded(
+        child: Container(
+         width: double.infinity,
+        alignment: alignmentIndex == 0
+          ? Alignment.centerLeft
+          : alignmentIndex == 1
+            ? Alignment.center
+            : Alignment.centerRight,
+
+        child: Text(
+         "Hola Flutter",
+          style: TextStyle(
+          fontSize: fontSize,
+          color: color,
+            fontWeight:
+             isBold ? FontWeight.bold : FontWeight.normal,
+            fontStyle:
+              isItalic ? FontStyle.italic : FontStyle.normal,
         ),
       ),
+    ),
+  ),
 
       // Controles
       Expanded(
         child: Column(
          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              "fontSize: 38",
+             Text(
+              "fontSize: ${fontSize.toInt()}",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
               ),
             ),
             Slider(
-              value: 38,
+              value: fontSize,
               min: 10,
               max: 60,
-              onChanged: (value) {},
+              onChanged: (value) {
+                setState(() {
+                  fontSize  = value;
+                });
+              },
             ),
 
-            const SizedBox(height: 5),
+            const SizedBox(height: 0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-
                 const Text(
                   "Bold:",
                   style: TextStyle(
@@ -65,13 +87,17 @@ class _ElementosUI extends State<ElementosUI> {
                   ),
                 ),
 
-                const SizedBox(width: 0),
+                const SizedBox(width: 10),
                 Switch(
-                  value: false,
-                  onChanged: (value) {},
+                  value: isBold,
+                  onChanged: (value) {
+                    setState(() {
+                      isBold = value;
+                    });
+                  },
                 ),
 
-                const SizedBox(width: 0),
+                const SizedBox(width: 10),
                 const Text(
                   "Italic:",
                   style: TextStyle(
@@ -80,10 +106,14 @@ class _ElementosUI extends State<ElementosUI> {
                   ),
                 ),
 
-                const SizedBox(width: 0),
+                const SizedBox(width: 5),
                 Switch(
-                  value: false,
-                  onChanged: (value) {},
+                  value: isItalic,
+                  onChanged: (value) {
+                    setState(() {
+                      isItalic = value;
+                    });
+                  },
                 ),
               ],
             ),
@@ -91,8 +121,16 @@ class _ElementosUI extends State<ElementosUI> {
             const SizedBox(height: 10),
             Center(
               child: ToggleButtons(
-                isSelected: const [false, true, false],
-                onPressed: (index) {},
+                isSelected:  [
+                  alignmentIndex == 0,
+                  alignmentIndex == 1,
+                  alignmentIndex == 2,
+                  ],
+                onPressed: (index) {
+                  setState(() {
+                    alignmentIndex = index;
+                  });
+                },
                 children: const [
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 20),
@@ -125,7 +163,11 @@ class _ElementosUI extends State<ElementosUI> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      color = Colors.red;
+                    });
+                  },
                   icon: const Icon(
                     Icons.circle,
                     size: 50,
@@ -134,7 +176,11 @@ class _ElementosUI extends State<ElementosUI> {
                 ),
 
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      color = Colors.black;
+                    });
+                  },
                   icon: const Icon(
                     Icons.circle,
                     size: 50,
@@ -143,7 +189,11 @@ class _ElementosUI extends State<ElementosUI> {
                 ),
 
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      color = Colors.green;
+                    });
+                  },
                   icon: const Icon(
                     Icons.circle,
                     size: 50,
@@ -152,7 +202,11 @@ class _ElementosUI extends State<ElementosUI> {
                 ),
 
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      color = Colors.blue;
+                    });
+                  },
                   icon: const Icon(
                     Icons.circle,
                     size: 50,
